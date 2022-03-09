2015a8a41cee09dcf61c368b9f1153491a4f4e32
rfbeier <robert.f.beier@gmail.com>
2022-03-09 18:47:19 -0500

    bare bones createApp setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import * as React from 'react';
 import { hot } from "react-hot-loader/root";
 
-export default function App() {
-
-  return (
-    <div>
-      app
-    </div>
-  );
+interface Props {
+   name:
+    string
 }
+
+class App extends React.Component<Props> {
+  render() {
+    const { name } = this.props;
+    return (
+      <>
+        <h1 className="text-4xl text-white bg-black">
+          Hello {name}
+        </h1>
+      </>
+    );
+  }
+}
+
+export default hot(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render } from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from "react-dom";
+
 import App from './App';
 import "./styles.css";
 
-render(
-  <App />,
-  document.getElementById('app'),
-);
+var mountNode = document.getElementById("app");
+ReactDOM.render(<App name="Jane" />, mountNode);
