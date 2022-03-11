a85f79b2e2a5340df978eb9f6430d6f43ac38659
rfbeier <robert.f.beier@gmail.com>
2022-03-10 22:16:14 -0500

    applied grid format to sidebar and containers. about to try Links in sidebar

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -7,17 +7,18 @@ const Sidebar = () => {
     let displayedBoxes = [];
     const buttonText = ["Connect Cluster", "Health Metrics", "Component Diagrams"]
     for (let i = 0; i<3; i+=1){
+        // put in cool shadows?
         if (i===tabSelected){
-            displayedBoxes.push(<div className="bg-teal-200 rounded-2xl text-center p-5 m-5 hover:bg-teal-300 hover:rounded-xl transition-all duration-300" onClick={() => setTabSelected(i)}> {buttonText[i]} </div>)
+            displayedBoxes.push(<div className="bg-teal-500 rounded-2xl text-center px-10 py-5 m-5 hover:bg-teal-300 hover:rounded-xl transition-all duration-300" onClick={() => setTabSelected(i)}> {buttonText[i]} </div>)
         } else{
-            displayedBoxes.push(<div className="bg-teal-200 rounded-2xl text-center p-5 m-5  hover:bg-teal-300 hover:rounded-xl transition-all duration-300" onClick={() => setTabSelected(i)}> {buttonText[i]} </div>)
+            displayedBoxes.push(<div className="bg-teal-200 rounded-2xl text-center px-10 py-5 m-5  hover:bg-teal-300 hover:rounded-xl transition-all duration-300" onClick={() => setTabSelected(i)}> {buttonText[i]} </div>)
         }
     }
     console.log(tabSelected)
     return(
-        <div className="fixed bg-darkTeal top-20 left-0 h-screen w-40 flex flex-col text-white shadow-lg">
-            {displayedBoxes}
-        </div>
+            <div className="bg-backgroundC-100 w-full flex flex-col shadow-lg" >{/*"fixed bg-blue-800 top-20 left-0 h-screen grow flex flex-col text-white shadow-lg">*/}
+                {displayedBoxes}
+            </div>       
     )
 
 }
