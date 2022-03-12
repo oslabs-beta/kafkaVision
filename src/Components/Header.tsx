7bda6b8476f1ac61a89d4245a1fcbef263753a0f
Isabelle <isabellesalvador@gmail.com>
2022-03-12 01:20:48 -0500

    changed colors, fixed header, added sign in button

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,16 +3,18 @@ import React, {useState} from 'react';
 const Header = () => {
   return (
     // header
-    <div className="bg-backgroundC-400 flex justify-between items-center h-15">
+    <div className="bg-darkIndigo flex justify-between items-center h-15">
 
       {/* logo */}
       {/* <div className="justify-items-end py-5 px-3 font-bold text-textC-100/75 text-3xl">kafkavision</div> */}
       {/* <div className="justify-items-end py-5 px-3 text-textC-100/90 drop-shadow-md font-bold bg-backgroundC-400 text-3xl">kafkavision</div> */}
-      <div className="bg-clip-text text-transparent py-4 px-3 bg-gradient-to-r from-backgroundC-200 to-teal-500 text-5xl font-black text-justify font-logo">kafkaVision</div>
+      <div className="bg-clip-text text-transparent py-4 px-3 bg-gradient-to-r from-slateBlue via-seafoam/75 to-slateBlue text-5xl font-black text-justify font-logo">kafkaVision</div>
 
       {/* github button */}
       <div className="flex justify-right">
-        <a href="https://github.com/oslabs-beta/kafkavision" className="py-1.5 px-3 mr-3 bg-buttonC-300 hover:bg-buttonC-200 text-textC-100 text-base rounded shadow hover:text-backgroundC-200 hover:rounded-xl transition-all duration-300">Github</a> 
+        <a href="https://github.com/oslabs-beta/kafkavision" className="py-1.5 px-3 mr-3 bg-seafoam/75 hover:bg-darkIndigo/50 text-zinc-900 text-base border border-white/50 rounded drop-shadow hover:text-seafoam/75 transition-all duration-300">Github</a> 
+
+        <a href="https://github.com/oslabs-beta/kafkavision" className="py-1.5 px-3 mr-3 bg-seafoam hover:bg-darkIndigo/50 text-zinc-900 text-base border border-white/50 rounded drop-shadow hover:text-seafoam/75 transition-all duration-300">Sign In</a>
 
         {/* dark mode toggle button  */}
       </div>  
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,25 +6,33 @@ module.exports = {
       colors: {
         transparent: 'transparent',
         current: 'currentColor',
-        'textC': {        //cool toned gray
-          100: '#c2d3e8', //main
-          200: '#9badc6', 
-          300: '#64748b', //clicked?
-        },
-        'buttonC': {      //teal
-          100: '#80ccc9', //hover
-          200: '#44aaa7', 
-          300: '#067c7a', //clicked
-        }, 
-        'backgroundC' : { //navy
-          100: '#548da8', 
-          200: '#275c75',
-          300: '#203549',
-          400: '#03223f', 
-        },
-        'accentC': {      //lilac
-          100: '#856fa8', // ?? not sure yet 
-        }  
+        limeGreen: '#d2fdbb',
+        seafoam: '#03dac5',
+        fontGray: '#D9E2EC',
+        lightGray: '#455a6d',
+        slateBlue: '#22404c',
+        darkIndigo: '#1d3051',
+        darkBlue: '#152D49'
+
+        // 'textC': {        //lilac toned gray
+        //   100: '#D9E2EC', 
+        //   // 200: '#9badc6', 
+        //   // 300: '#64748b', 
+        // },
+        // 'buttonC': {      //teal
+        //   100: '#80ccc9', //hover
+        //   200: '#44aaa7', 
+        //   300: '#067c7a', //clicked
+        // }, 
+        // 'backgroundC' : { //navy
+        //   100: '#548da8', 
+        //   200: '#275c75',
+        //   300: '#203549',
+        //   400: '#03223f', 
+        // },
+        // 'accentC': {      //lilac
+        //   100: '#856fa8', // ?? not sure yet 
+        // }  
       },
       fontFamily: {
         logo: ['Hubballi', 'sans-serif']
@@ -33,3 +41,18 @@ module.exports = {
   },
   plugins: [],
 };
+
+
+/*
+limeGreen: '#d2fdbb',
+seafoam: '#03dac5',
+
+fontGray: '#D9E2EC',
+lightGray: '#455a6d',
+slateBlue: '#22404c'
+darkIndigo: '#1d3051',
+darkBlue: '#152D49'
+
+// default background
+ gray-50
+*/
