e29f49cfa891894d924ddb2868e3b9f99cd425cd
Neel <neelanjan.lakshman@gmail.com>
2022-03-15 19:00:09 -0400

    pushing branch

--- server/models/sessionModel.ts
+++ server/models/sessionModel.ts
@@ -1,23 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
-
 interface Sessions {
-    username: string;
-    createdAt: Date;
+  username: string;
+  createdAt: Date;
 }
 
 const SessionSchema = new Schema<Sessions>({
-    username: {
-        type: String,
-        required: true,
-    },
-    createdAt: {
-        type: Date,
-        default: new Date(),
-    }
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
 });
 
-const Session = model<Sessions>('Session', SessionSchema);
+const Session = model<Sessions>("Session", SessionSchema);
 
 export default Session;
