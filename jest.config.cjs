e88315caedfc9b1caa3c5f775f15c05535fc9939
Kayliegh.Hill <kayliegh.hill@gmail.com>
2022-03-26 13:30:08 -0400

    commented jest config files to provide context

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,19 +1,38 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+// use ts-jest to compile ts to js with ESM support, perform type check, and execute compiled js
+
 module.exports = {
-  preset: 'ts-jest',
+  // .ts, .tsx, .js, .jsx, .mjs files will be transformed with ts-jest to ESM syntax:
+  preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  // passing empty transform prevents babel from transforming files:
   transform: {
-    '^.+\\.js$': 'babel-jest',
+    // '^.+\\.js$': 'babel-jest',
   },
+  // required global settings to use ESM with ts-jest:
   globals: {
     'ts-jest': {
+      diagnostics: true,
       useESM: true,
-      stringifyContentPathRegex: '\\.(html|svg)$',
       tsconfig: './tsconfig.json',
     },
   },
-  // globalSetup: './__tests__/jest-setup.js',
-  // globalTeardown: './__tests__/jest-teardown.js',
+  globalSetup: './__tests__/jest-setup.js',
+  globalTeardown: './__tests__/jest-teardown.js',
+  // troubleshooting steps from ts-jest https://github.com/kulshekhar/ts-jest/blob/main/TROUBLESHOOTING.md
+  // cannot find module "" from ""
+  // {NOT WORKING - possible bug solved in new version: https://github.com/facebook/jest/issues/9771}
+  roots: ['.'],
+  moduleDirectories: ['./node_modules'],
+  modulePaths: ['./chart.js'],
+
+  // jest -- module mapping:
   
-  extensionsToTreatAsEsm: ['.tsx'],
+
+  // if problem is chart.js files need to be transformed, this should allow chartjs module to be transformed and executed by ts-jest {NOT WORKING - suspect this is not the issue}
+  transformIgnorePatterns: ['./node_modules/(?!(chart.js|react-chartjs-2)/)'],
+  // if not using preset, pass files to use ESM syntax here:
+  //extensionsToTreatAsEsm: ['.tsx', '.ts'],
 };
