131c1c0267015fecafbf11fda484dd1ceec21836
Isabelle <isabellesalvador@gmail.com>
2022-03-28 20:33:19 -0400

    collapisble window working, modularized components

--- src/Containers/HealthMetricsContainer.tsx
+++ src/Containers/HealthMetricsContainer.tsx
@@ -1,126 +1,28 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
-import HealthMetricsChart from '../chartComponents/HealthMetricsChart';
 import { appContext } from '../App';
+import HealthMetricsChart from '../chartComponents/HealthMetricsChart';
+import StaticHealthData from '../Components/StaticHealthData'
 
 const HealthMetricsContainer = () => {
-  //UNPACK APP STATE TO GET PROMETHEUS URL
+  //UNPACK APP STATE
   const {
     state: { connectionState },
     actions: { setGlobalState },
   } = useContext(appContext);
 
-  const queryParams = 'api/v1/query?query=';
-  const queryLink = connectionState.url_prometheus + queryParams;
   const connectionStatus = connectionState.isConnected; // semantic variable
 
-  // LOCAL STATE
-  const [topics, setTopics] = useState(0);
-  const [controllers, setControllers] = useState(0);
-  const [brokers, setBrokers] = useState(0);
-  const [partitions, setPartitions] = useState(0);
-  const [underReplicated, setUnderReplicated] = useState(0);
-  const [offlinePartitions, setOfflinePartitions] = useState(0);
-
-  //CONTROLLERS
-  useEffect(() => {
-    //active controllers query
-    const query = 'kafka_controller_kafkacontroller_activecontrollercount';
-    fetch(queryLink + query)
-      .then((data) => data.json())
-      .then((result) => {
-        // console.log('CONTROLLERS QUERY DATA: ', result);
-        // console.log('CHECKING VALUE: ', result.data.result[0].value[1]);
-        setControllers(result.data.result[0].value[1]);
-      })
-      .catch((err) => {
-        console.log('ERROR IN CONTROLLERS USEEFFECT: ', err);
-      });
-  }, [controllers]);
-
-  //BROKERS
-  useEffect(() => {
-    //brokers online query
-    const query = 'count(kafka_server_replicamanager_leadercount)';
-    fetch(queryLink + query)
-      .then((data) => data.json())
-      .then((result) => {
-        // console.log('BROKERS QUERY DATA: ', result);
-        // console.log('CHECKING VALUE: ', result.data.result[0].value[1]);
-        setBrokers(result.data.result[0].value[1]);
-      })
-      .catch((err) => {
-        console.log('ERROR IN BROKERS USEEFFECT: ', err);
-      });
-  }, [brokers]);
-
-  //PARTITION COUNT
-  useEffect(() => {
-    const query = 'sum(kafka_controller_kafkacontroller_globalpartitioncount)';
-    fetch(queryLink + query)
-      .then((data) => data.json())
-      .then((result) => {
-        // console.log('PARTITION COUNT QUERY: ', result);
-        setPartitions(result.data.result[0].value[1]);
-      })
-      .catch((err) => {
-        console.log('ERROR IN PARTITION COUNT USEEFFECT: ', err);
-      });
-  }, [partitions]);
-
-  //TOPIC COUNT
-  useEffect(() => {
-    const query = 'sum(kafka_controller_kafkacontroller_globaltopiccount)';
-    fetch(queryLink + query)
-      .then((data) => data.json())
-      .then((result) => {
-        // console.log('TOPICS COUNT QUERY: ', result);
-        setTopics(result.data.result[0].value[1]);
-      })
-      .catch((err) => {
-        console.log('ERROR IN TOPICS COUNT USEEFFECT: ', err);
-      });
-  }, [topics]);
-
   //TOPICS DROP DOWN MENU
-  const options = [];
-  for (let i = 1; i <= topics; i += 1) {
-    options.push(
-      <option value="topic{i}" key={i}>
-        {' '}
-        Topic {i}{' '}
-      </option>
-    );
-  }
-
-  //UNDERREPLICATED PARTITIONS
-  useEffect(() => {
-    const query = 'sum(kafka_server_replicamanager_underreplicatedpartitions)';
-    fetch(queryLink + query)
-      .then((data) => data.json())
-      .then((result) => {
-        // console.log('UNDER REPLICATED PARTITIONS QUERY: ', result);
-        setUnderReplicated(result.data.result[0].value[1]);
-      })
-      .catch((err) => {
-        console.log('ERROR IN UNDER REPLICATED PARTITIONS USEEFFECT: ', err);
-      });
-  }, [underReplicated]);
-
-  //OFFLINE PARTITIONS COUNT
-  useEffect(() => {
-    const query =
-      'sum(kafka_controller_kafkacontroller_offlinepartitionscount)';
-    fetch(queryLink + query)
-      .then((data) => data.json())
-      .then((result) => {
-        // console.log('OFFLINE PARTITIONS QUERY: ', result);
-        setOfflinePartitions(result.data.result[0].value[1]);
-      })
-      .catch((err) => {
-        console.log('ERROR IN OFFLINE PARTITIONS USEEFFECT: ', err);
-      });
-  }, [offlinePartitions]);
+  // const options = [];
+  // for (let i = 1; i <= topics; i += 1) {
+  //   options.push(
+  //     <option value="topic{i}" key={i}>
+  //       {' '}
+  //       Topic {i}{' '}
+  //     </option>
+  //   );
+  // }
 
   let renderedContent: any;
   if (connectionStatus === false) {
@@ -145,9 +47,9 @@ const HealthMetricsContainer = () => {
     );
   } else {
     renderedContent = (
-      <div className="text-xl text-center height-max m-10 border-2 border-limeGreen/70 rounded bg-backgroundC-400 text-fontGray/75 font-bold">
+      <div className="text-xl text-center m-10 border-2 border-limeGreen/70 rounded bg-backgroundC-400 text-fontGray/75 font-bold">
         <h2 className="m-4 text-center">Health Dashboard</h2>
-        <div className="border-2 border-seafoam/40 rounded m-5 grid grid-rows-2  bg-slateBlue/50">
+        <div className="border-2 border-seafoam/40 rounded m-5 flex flex-col bg-slateBlue/50">
           {/* Overall Cluster Health */}
           <div className="rounded m-5 border border-fontGray/50 bg-zinc-800">
             <p className="m-3">Overall Cluster Health</p>
@@ -155,59 +57,8 @@ const HealthMetricsContainer = () => {
               <HealthMetricsChart />
             </div>
           </div>
-
-          {/* Topic Metrics */}
-          <div className='border border-slateBlue rounded m-5 bg-zinc-800'>
-            <p className='m-5'>Topic Metrics</p>
-
-            {/* List of metrics */}
-            <div className="grid grid-cols-3  rounded gap-5 p-5 text-sm font-light divide-fontGray/50">
-              <div className="bg-slateBlue/70 p-5 rounded border border-fontGray/50">
-                <div className="text-xs text-seafoam/70 font-bold">
-                  Global Topic Count:{' '}
-                </div>
-                <div className="text-7xl text-limeGreen/80">{topics} </div>
-              </div>
-
-              <div className="bg-slateBlue/70 p-5 rounded border ">
-                <div className="text-xs text-seafoam/70 font-bold">
-                  Global Online Partitions:{' '}
-                </div>
-                <div className="text-7xl text-limeGreen/80">{partitions} </div>
-              </div>
-
-              <div className="bg-slateBlue/70 p-5 rounded border">
-                <div className="text-xs text-seafoam/70 font-bold">
-                  Active Controllers:{' '}
-                </div>
-                <div className="text-7xl text-limeGreen/80">{controllers} </div>
-              </div>
-
-              <div className="bg-slateBlue/70 p-5 rounded border">
-                <div className="text-xs text-seafoam/70 font-bold">
-                  Brokers Online:{' '}
-                </div>
-                <div className="text-7xl text-limeGreen/80">{brokers} </div>
-              </div>
-
-              <div className="bg-slateBlue/70 p-5 rounded border">
-                <div className="text-xs text-seafoam/70 font-bold">
-                  Under Replicated Partitions:{' '}
-                </div>
-                <div className="text-7xl text-limeGreen/80">
-                  {underReplicated}{' '}
-                </div>
-              </div>
-
-              <div className="bg-slateBlue/70 p-5 rounded border">
-                <div className="text-xs text-seafoam/70 font-bold">
-                  Offline Partitions:{' '}
-                </div>
-                <div className="text-7xl text-limeGreen/80">
-                  {offlinePartitions}{' '}
-                </div>
-              </div>
-            </div>
+          <div>
+            <StaticHealthData />
           </div>
         </div>
       </div>
--- src/Containers/TopicsContainer.tsx
+++ src/Containers/TopicsContainer.tsx
@@ -15,30 +15,45 @@ interface Props {
 
 //changed to any bc when Props are passed in error: TS2739: Type '{}' is missing the following properties from type 'Props': key, onClick, parentRef, style
 const TopicsContainer: React.FC <any>= () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState([false, false, false, false, false]);
   const parentRef = useRef<HTMLDivElement>(null);
 
-  const collapse: any = isOpen ? 'h-70 scale-100 border border-green-300' : 'h-0 scale-0';
 
+  const helperFunction = (i: number) => {
+    setIsOpen((prevState) => {
+      prevState[i] = !prevState[i];
+      return {...prevState};
+    })
+  };
+ 
   const topics = [];
   for (let i = 0; i < 5; i++){
+    let collapse: string = isOpen[i] ? 'h-70 scale-100 border border-green-300 transition-all duration-300' : 'h-0 scale-0 transition-all duration-300';
+ 
+
     topics.push(
-      //added parentRef => supposed to be able to open one at a time?
-      <div className='border border-red-900' ref={parentRef}> 
-      <button onClick={() => {
-        console.log('clicked');
-        console.log('height, ', collapse);
-        setIsOpen(!isOpen)}}>
-        Topic: {i}
-      </button>
-      <div className={collapse}>
-        <div className='border border-white m-5 h-70'>
-          <TopicsChart/>
+      <div className='border border-red-900 m-0 p-0' ref={parentRef} onClick={()=> helperFunction(i)}> 
+        <div className="cursor-default m-0 p-0" key={i}>
+          Topic: {i+1}
+        </div>
+        {/* <button 
+          key={i}
+          onClick={() => {
+            console.log(i, 'clicked');
+            helperFunction(i);
+          }}> */}
+              
+        {/* </button> */}
+        <div className={collapse}>
+          <div className='border border-white m-5 h-70'>
+            <TopicsChart/>
+          </div>
         </div>
       </div>
-    </div>
     )
-  }
+  };
+
+  
 
   return (
     <div>
